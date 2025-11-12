242762be0e5e1227618fb7529d12774ddb7ce8ce
Varnith2007 <saivarnith0@gmail.com>
2025-11-12 10:32:05 +0530

    Create natural number. .py

---
+++ natural number. .py
@@ -0,0 +1,13 @@
+num=int(input("enter the number"))
+if num<0:
+   print("enter the number")
+else:
+   sum_number=0
+   count=1
+   while count<=num:
+    sum_number += count
+    count += 1
+print("the sum of natural number is:",sum_number)
+
+
+
